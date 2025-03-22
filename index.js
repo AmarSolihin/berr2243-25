d36b7e854cd695ded373d4427a7378092c888759
Amar <amarsolihin2331@gmail.com>
2025-03-22 15:46:46 +0800

    Add NodeJS script and MongoDB connection

---
+++ index.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB!");
+
+        const db = client.db("testDB");
+        const users = db.collection("users");
+
+        const result = await users.insertOne({ name: "AmarSolihin", age: 22 });
+        console.log(`Document inserted with _id: ${result.insertedId}`);
+
+        const findResult = await users.findOne({ _id: result.insertedId });
+        console.log("Found Document:", findResult);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+}
+
+run();
